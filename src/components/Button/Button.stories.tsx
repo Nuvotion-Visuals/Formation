8d28f60335afeb77c8cdf3586a886a8b3457f304
Tom <tom-leamon>
2022-08-06 04:57:21 -0500

    Remove storybook example

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,7 +5,7 @@ import Button from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Foundation/Button',
+  title: 'Formation/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof Button>;
