c45542ace72ba32bb10e120f0bf3514f0f70a72c
Tom <tom-leamon>
2022-08-08 09:02:35 -0500

    Add LoadingSpinner

---
+++ src/components/LoadingSpinner/LoadingSpinner.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { LoadingSpinner } from './LoadingSpinner'
+
+export default {
+  title: 'Formation/LoadingSpinner',
+  component: LoadingSpinner,
+} as ComponentMeta<typeof LoadingSpinner>
+
+const Template: ComponentStory<typeof LoadingSpinner> = (args) => <LoadingSpinner {...args} />
+
+export const Regular = Template.bind({})
+Regular.args = {
+  
+}
+
+export const Small= Template.bind({})
+Small.args = {
+  small: true
+}
