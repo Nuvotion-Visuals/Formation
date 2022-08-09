6969b2ee890db4b2d82a8cf8def5f5d4b5573743
Tom <tom-leamon>
2022-08-08 23:49:21 -0500

    Replace remaining absolute imports with relative imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { Button } from 'components/Button/Button'
-export { Icon } from 'components/Icon/Icon'
-export { Tabs } from 'components/Tabs/Tabs'
-export { Box } from 'components/Box/Box'
+export { Button } from './Button/Button'
+export { Icon } from './Icon/Icon'
+export { Tabs } from './Tabs/Tabs'
+export { Box } from './Box/Box'
