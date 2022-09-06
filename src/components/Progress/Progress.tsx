693027eae8026cbb84c35a49b23a15a083813bb5
Ryan Gorgol <ryangorgol@gmail.com>
2022-09-06 13:29:18 -0700

    basic functionality has been implemented. maximum and value props have been implemented.

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,22 +1,44 @@
 import styled from 'styled-components'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
+  minimum?: number,
+  maximum: number,
+  value: number,
 }
 
-export const Progress = ({  }: Props) => {
-    
+export const Progress = ({ minimum, maximum, value }: Props) => {
+  
+  const [status, setStatus] = useState<number>(0)
+
+  useEffect(() => {
+    if(maximum !== undefined)
+      setStatus(value / maximum * 100)
+      console.log(status, ":status")
+  }, [value])
+
   return (
-        <S.Container>
-          
-        </S.Container>
-      );
+    <S.Container>
+      <S.Slider
+         status={status}   
+      />
+    </S.Container>
+  );
 };
   
 const S = {
   Container: styled.div<{}>`
-    background: red;
-    width: 10rem;
-    height: 10rem;
+    height: 0.5rem;
+    width: 100%;
+    border-radius: 0.25rem;
+    background: var(--F_Surface);
+  `,
+  Slider: styled.div<{
+    status: number
+  }>`
+    height: 100%;
+    width: ${props => `${props.status}%`};
+    background: var(--F_Primary);
+    border-radius: 0.5rem;
   `
 }
