dcd5c8b6a59379df016933cc1d825da251d25dca
tom-leamon <leamon@outlook.com>
2022-09-14 00:48:50 -0500

    Add Link component

--- src/internal.ts
+++ src/internal.ts
@@ -75,6 +75,7 @@ import { Ripple } from './components/Ripple/Ripple'
 import { Spacer } from './components/Spacer/Spacer'
 import { StyleHTML } from './components/StyleHTML/StyleHTML'
 import { Docking } from './components/Docking/Docking'
+import { Link } from './components/Link/Link'
 
 export {
   AspectRatio,
@@ -102,7 +103,8 @@ export {
   Ripple,
   Spacer,
   StyleHTML,
-  Docking
+  Docking,
+  Link
 }
 
 // Molecules (1)
