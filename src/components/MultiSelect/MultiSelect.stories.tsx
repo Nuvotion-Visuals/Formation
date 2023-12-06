c18577df7d80d5268908bfe8dad705ff6f63048b
tom-leamon <leamon@outlook.com>
2023-12-06 17:49:02 -0600

    Move managment of selected items to parent in MultiSelect, use indicies isntead of Set, add ContextMenu

--- src/components/MultiSelect/MultiSelect.stories.tsx
+++ src/components/MultiSelect/MultiSelect.stories.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { MultiSelect } from './MultiSelect'
+import { AspectRatio } from '../AspectRatio/AspectRatio'
+import { Box } from '../Box/Box'
 
 export default {
-  title: 'Components/MultiSelect',
+  title: 'Input/MultiSelect',
   component: MultiSelect,
 } as ComponentMeta<typeof MultiSelect>
 
-const Template: ComponentStory<typeof MultiSelect> = (args) => (
-  <MultiSelect />
-)
+const Template: ComponentStory<typeof MultiSelect> = (args) => {
+  const [items, setItems] = useState(Array.from({ length: 10 }, (_, i) => `Item ${i + 1}`))
+  const [selectedIndices, setSelectedIndices] = useState<number[]>([])
+
+    const handleDelete = () => {
+      const newItems = items.filter((_, index) => {
+          return !selectedIndices.includes(index);
+      });
+      setItems(newItems);
+      setSelectedIndices([]);
+  };
+
+  return (
+    <MultiSelect 
+      {...args}
+      selectedIndices={selectedIndices} 
+      setSelectedIndices={setSelectedIndices}
+      menuOptions={{
+        items: [
+          {
+            text: 'Remove',
+            onClick: () => handleDelete(),
+          }
+        ],
+      }}
+    >
+      {items.map((item) => (
+        <Box width={8}>
+          <AspectRatio ratio={16/9}>
+            <p>{item}</p>
+          </AspectRatio>
+        </Box>
+      ))}
+    </MultiSelect>
+  )
+}
 
 export const Default = Template.bind({})
-Default.args = {
-  // Add any default props if required
-}
 Default.parameters = {
   layout: 'fullscreen'
 }
--- src/components/MultiSelect/MultiSelect.tsx
+++ src/components/MultiSelect/MultiSelect.tsx
@@ -1,9 +1,15 @@
-import { Gap } from '../../internal'
-import React, { useState, useRef, useEffect } from 'react'
+import { ContextMenu, DropdownProps, Gap } from '../../internal'
+import React, { ReactNode, useState, useRef, useEffect } from 'react'
 import styled from 'styled-components'
 
-export const MultiSelect = () => {
-  const [selectedItems, setSelectedItems] = useState(new Set<number>())
+type PropType = {
+  children: React.ReactElement[],
+  selectedIndices: number[],
+  setSelectedIndices: (indices: number[]) => void,
+  menuOptions: DropdownProps,
+}
+
+export const MultiSelect = ({ children, selectedIndices, setSelectedIndices, menuOptions }: PropType) => {
   const [isDragging, setIsDragging] = useState(false)
   const [dragStart, setDragStart] = useState({ x: 0, y: 0 })
   const [dragEnd, setDragEnd] = useState({ x: 0, y: 0 })
@@ -21,7 +27,7 @@ export const MultiSelect = () => {
       
       if (e.key === "Escape") {
         setIsDragging(false)
-        setSelectedItems(new Set<number>())
+        setSelectedIndices([])
       }
     }
 
@@ -42,24 +48,38 @@ export const MultiSelect = () => {
 
   const handleItemMouseDown = (e: React.MouseEvent, index: number) => {
     e.stopPropagation()
-    const newSelectedItems = new Set(selectedItems)
-    if (ctrlCmdPressed.current) {
-      if (newSelectedItems.has(index)) {
-        newSelectedItems.delete(index)
-      } else {
-        newSelectedItems.add(index)
+    
+    let newSelectedIndices = [...selectedIndices]
+    
+    if (e.button === 2) { // Check if right-click
+      if (!selectedIndices.includes(index)) { // Check if the item is already selected
+        newSelectedIndices = [index]
         setLastSelected(index)
       }
-    } else if (shiftPressed.current && lastSelected != null) {
-      for (let i = Math.min(lastSelected, index); i <= Math.max(lastSelected, index); i++) {
-        newSelectedItems.add(i)
+    } 
+    else {
+      if (ctrlCmdPressed.current) {
+        const indexPosition = newSelectedIndices.indexOf(index)
+  
+        if (indexPosition !== -1) {
+          newSelectedIndices.splice(indexPosition, 1)
+        } else {
+          newSelectedIndices.push(index)
+          setLastSelected(index)
+        }
+      } else if (shiftPressed.current && lastSelected != null) {
+        newSelectedIndices = []
+  
+        for (let i = Math.min(lastSelected, index); i <= Math.max(lastSelected, index); i++) {
+          newSelectedIndices.push(i)
+        }
+      } else {
+        newSelectedIndices = [index]
+        setLastSelected(index)
       }
-    } else {
-      newSelectedItems.clear()
-      newSelectedItems.add(index)
-      setLastSelected(index)
     }
-    setSelectedItems(newSelectedItems)
+  
+    setSelectedIndices(newSelectedIndices)
     e.preventDefault()
   }
 
@@ -68,13 +88,12 @@ export const MultiSelect = () => {
     setDragStart({ x: e.clientX - rect.left, y: e.clientY - rect.top })
     setDragEnd({ x: e.clientX - rect.left, y: e.clientY - rect.top })
     setIsDragging(true)
-
-    // Reset selected items when dragging begins
-    setSelectedItems(new Set<number>())
+    setSelectedIndices([])
   }
 
   const handleMouseMove = (e: React.MouseEvent) => {
     if (!isDragging) return
+
     const rect = containerRef.current!.getBoundingClientRect()
     setDragEnd({ x: e.clientX - rect.left, y: e.clientY - rect.top })
     updateSelection()
@@ -83,7 +102,7 @@ export const MultiSelect = () => {
   const handleMouseUp = () => {
     setIsDragging(false)
   }
-  
+
   const updateSelection = () => {
     const rect = containerRef.current!.getBoundingClientRect()
     const selectionRect = {
@@ -93,7 +112,7 @@ export const MultiSelect = () => {
       right: Math.max(dragStart.x, dragEnd.x),
     }
 
-    const newSelectedItems = new Set<number>()
+    const newSelectedIndices: number[] = [...selectedIndices];
     itemRefs.current.forEach((itemRef, index) => {
       if (itemRef) {
         const itemRect = itemRef.getBoundingClientRect()
@@ -103,18 +122,17 @@ export const MultiSelect = () => {
           left: itemRect.left - rect.left,
           right: itemRect.right - rect.left,
         }
-        if (
-          itemRelativeRect.top < selectionRect.bottom &&
-          itemRelativeRect.bottom > selectionRect.top &&
-          itemRelativeRect.left < selectionRect.right &&
-          itemRelativeRect.right > selectionRect.left
-        ) {
-          newSelectedItems.add(index)
+
+        if (itemRelativeRect.top < selectionRect.bottom &&
+            itemRelativeRect.bottom > selectionRect.top &&
+            itemRelativeRect.left < selectionRect.right &&
+            itemRelativeRect.right > selectionRect.left) {
+              newSelectedIndices.push(index)
         }
       }
     })
 
-    setSelectedItems(newSelectedItems)
+    setSelectedIndices(newSelectedIndices)
   }
 
   return (
@@ -124,20 +142,38 @@ export const MultiSelect = () => {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
     >
+       <ContextMenu
+            dropdownProps={{
+              ...menuOptions,
+              items: menuOptions.items.map(item => ({
+                ...item,
+                onClick: (e) => {
+                  if (item.onClick) {
+                    item.onClick(e)
+                  }
+                },
+              })),
+            }}
+          >
       <Gap>
       {
-        Array.from({ length: 10 }, (_, i) => (
-          <S.Item
-            key={i}
-            ref={(el) => (itemRefs.current[i] = el)}
-            selected={selectedItems.has(i)}
-            onMouseDown={(e) => handleItemMouseDown(e, i)}
-          >
-            Item {i + 1}
-          </S.Item>
+        React.Children.map(children, (child, i) => (
+         
+            <S.Item
+              key={i}
+              ref={(el) => (itemRefs.current[i] = el)}
+              selected={selectedIndices.includes(i)}
+              onMouseDown={(e) => handleItemMouseDown(e, i)}
+            >
+              {
+                child
+              }
+            </S.Item>
         ))
       }
       </Gap>
+      </ContextMenu>
+
       {
         isDragging && (
           <S.SelectionBox
@@ -161,7 +197,6 @@ const S = {
     padding: 1rem;
   `,
   Item: styled.div<{ selected: boolean }>`
-    padding: 10px;
     background: ${(props) => (props.selected ? 'rgba(0, 123, 255, 0.2)' : 'none')};
     border: 1px solid darkgrey;
     width: 100%;
